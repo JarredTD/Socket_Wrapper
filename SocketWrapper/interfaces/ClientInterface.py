7a4b49a58612c01943ead8f5caed6b4c6edbdf37
JarredTD <jarred.desrosiers@yahoo.com>
2023-04-24 19:52:56 -0400

    Fixed block comments

--- SocketWrapper/interfaces/ClientInterface.py
+++ SocketWrapper/interfaces/ClientInterface.py
@@ -1,22 +1,23 @@
 '''
 Imports the Socket interface that the Client interface is a child of
 '''
-from socket_interface.SocketInterface import SocketInterface
+from SocketInterface import SocketInterface
 
 ##############################
-'''
-Client Interface, contains methods that a client would need, regardless of implementation of the client
-such as connection to a socket.
-'''
+
 class ClientInterface(SocketInterface):
+    '''
+    Client Interface, contains methods that a client would need, regardless of implementation of the client
+    such as connection to a socket.
+    '''
     def __init__(self) -> None:
         super().__init__()
 
-    '''
-    Connects using the ip and port number. Should catch and OS error, and display the error, and 
-    should return false.
-    '''
     def _connect_socket(self) -> bool:
+        '''
+        Connects using the ip and port number. Should catch and OS error, and display the error, and 
+        should return false.
+        '''
         try:
             self.connect((self.ip, self.port_num))
         except OSError as e:
--- SocketWrapper/interfaces/ServerInterface.py
+++ SocketWrapper/interfaces/ServerInterface.py
@@ -4,21 +4,21 @@ Import Socket interface that the Server class is a child of
 from SocketInterface import SocketInterface
 
 ##############################
-'''
-Server Interface, contains methods that a server would need regardless of implementation, such as binding, and tracking the connection socket.
-'''
 class ServerInterface(SocketInterface):
+    '''
+    Server Interface, contains methods that a server would need regardless of implementation, such as binding, and tracking the connection socket.
+    '''
 
     def __init__(self) -> None:
         super().__init__()
     
-    '''
-    Binds using the ip and port number. Should catch and OS error, and display the error, and 
-    should return false.
-
-    An input state must be flipped to true, to indicate that on a class level, that this object has recieved valid input
-    '''
     def _bind_socket(self) -> bool:
+        '''
+        Binds using the ip and port number. Should catch and OS error, and display the error, and 
+        should return false.
+
+        An input state must be flipped to true, to indicate that on a class level, that this object has recieved valid input
+        '''
         try:
             super().bind((self.ip, self.port_num))
         except OSError as e:
@@ -28,8 +28,8 @@ class ServerInterface(SocketInterface):
         self.input_state = True
         return True
     
-    '''
-    The connection socket is tracked by the class through this method.
-    '''
     def connect_to_client(self, connectionSocket):
+        '''
+        connection socket is tracked by the class through this method.
+        '''
         self.connectionSocket = connectionSocket
