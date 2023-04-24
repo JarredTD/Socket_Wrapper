2f585868e7749df1378e0641426ef883ef4027b1
JarredTD <jarred.desrosiers@yahoo.com>
2023-04-24 19:49:24 -0400

    Fixed block comment

--- SocketWrapper/util/ClearScreen.py
+++ SocketWrapper/util/ClearScreen.py
@@ -1,9 +1,9 @@
 from os import system, name
 
-'''
-Checks what OS the user is on, and runs the appropriate command to clear the terminal
-'''
 def clear():
+    '''
+    Checks what OS the user is on, and runs the appropriate command to clear the terminal
+    '''
     if name == 'nt':
         _ = system('cls')
     else: # For mac and linux(here, os.name is 'posix')
