f8b43e8d03a4d872f996ca4b7b3a924e4d61e64e
JarredTD <jarred.desrosiers@yahoo.com>
2023-04-24 19:42:54 -0400

    Fixed block comments

--- SocketWrapper/Client.py
+++ SocketWrapper/Client.py
@@ -1,16 +1,16 @@
 '''
-Import the ClientInterface that the Client class is a child of
+Import the ClientInterface that the Client class is a child of 
 '''
 
 from interfaces.ClientInterface import ClientInterface
 
 ##############################
 
-'''
-Client class, child of ClientInterface. Used as a container for the collection of the client information and automatically
-using that information.
-'''
 class Client(ClientInterface):
+    '''
+    Client class, child of ClientInterface. Used as a container for the collection of the client information and automatically
+    using that information.
+    '''
     def __init__(self, ip: str, port: str) -> None:
         super().__init__()
         self.ip = ip
@@ -19,17 +19,17 @@ class Client(ClientInterface):
 
         self._parse_file()
     
-    '''
-    Runs as part of the initialization. Converts the port to a int. and then binds the socket.
-    '''
     def _parse_file(self) -> None:
+        '''
+        Runs as part of the initialization. Converts the port to a int. and then binds the socket.
+        '''
         if ((self._convert_port() == False) or (self._connect_socket() == False)):
             raise StopIteration
     
-    '''
-    Takes a msg and checks if it meets a requirement. If it does, false is returned.
-    '''
     def check_end_condition(self, msg:str):
+        '''
+        Takes a msg and checks if it meets a requirement. If it does, false is returned.
+        '''
         if (msg == "exit"):
             self.send_msg(msg)
             self.connectionSocket.close()
