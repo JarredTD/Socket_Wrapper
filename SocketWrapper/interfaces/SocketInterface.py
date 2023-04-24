37fbe8d12e75a64fe838877e14a40b8853d991c9
JarredTD <jarred.desrosiers@yahoo.com>
2023-04-24 19:50:50 -0400

    Fixed block commits

--- SocketWrapper/interfaces/SocketInterface.py
+++ SocketWrapper/interfaces/SocketInterface.py
@@ -1,11 +1,10 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
-'''
-Socket interface. Clients and servers are build around a sockets, and thus this can serve as a 
-parent class to clients and servers.
-'''
 class SocketInterface(socket):
-
+    '''
+    Socket interface. Clients and servers are build around a sockets, and thus this can serve as a 
+    parent class to clients and servers.
+    '''
     def __init__(self) -> None:
         super().__init__(AF_INET, SOCK_STREAM)
         self.input_state = False
@@ -13,14 +12,14 @@ class SocketInterface(socket):
         self.ip = None
         self.port_num = None
 
-    '''
-    Converts port that the socket was initialized with from a string to an int, or defaults the value if the input
-    is an empty string.
-
-    Should catch a TypeError if the type casting from str to int fails, and in the case of catching the type error
-    should return false.
-    '''
     def _convert_port(self) -> bool:
+        '''
+        Converts port that the socket was initialized with from a string to an int, or defaults the value if the input
+        is an empty string.
+
+        Should catch a TypeError if the type casting from str to int fails, and in the case of catching the type error
+        should return false.
+        ''' 
         if (self.port_num == ''): 
             self.port_num = 12000
         else: 
@@ -32,13 +31,13 @@ class SocketInterface(socket):
             
             return True
         
-    '''
-    Recieves a message using the connection socket. First takes an int, which is the size of the message.
-    Then it loops and recieves until it has the full message (comparing to the original length sent).
-
-    If an error occurs, throws StopIteration
-    '''
     def recv_msg(self) -> None:
+        '''
+        Recieves a message using the connection socket. First takes an int, which is the size of the message.
+        Then it loops and recieves until it has the full message (comparing to the original length sent).
+
+        If an error occurs, throws StopIteration
+        '''
         try:
             msg_recv = self.connectionSocket.recv(4)
             msg_len = int.from_bytes(msg_recv, byteorder='big')
@@ -51,11 +50,11 @@ class SocketInterface(socket):
 
         return msg.decode()
     
-    '''
-    Encodes the size of the message to the message itself, sends the entire "packet" through
-    its socket. If an error occurs, StopIteration is thrown.
-    '''
     def send_msg(self, msg: str) -> None:
+        '''
+        Encodes the size of the message to the message itself, sends the entire "packet" through
+        its socket. If an error occurs, StopIteration is thrown.
+        '''
         try:
             msg = msg.encode()
             msg_len = len(msg)
